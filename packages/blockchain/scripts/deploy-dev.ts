5533ce40b82dac6bd3330eff510277b2abc5922b
Guillaume Bibeau-laviolette <guillaumebibeau@filevine.com>
2021-12-02 14:52:52 -0500

    automating dev environment a bit

---
+++ packages/blockchain/scripts/deploy-dev.ts
@@ -0,0 +1,27 @@
+import { ethers } from "hardhat";
+import fs from "fs-extra";
+
+const CONFIG_PATH = "../config/config.json";
+
+async function main() {
+  const [owner, trusted] = await ethers.getSigners();
+
+  const Lotto = await ethers.getContractFactory("Lotto");
+  const lotto = await Lotto.deploy(trusted.address, owner.address);
+
+  await lotto.deployed();
+
+  const config = await fs.readJson(CONFIG_PATH);
+  config.dev.contracts.Lotto.address = lotto.address;
+
+  await fs.writeJson(CONFIG_PATH, config, { spaces: 2 });
+
+  console.log("Lotto deployed to:", lotto.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
