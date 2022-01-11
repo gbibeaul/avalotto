6eaea4899485bba1c6978dfe2ad11a9c9d8ed91b
Guillaume Bibeau-laviolette <g.bibeaulaviolette@gmail.com>
2022-01-10 19:30:09 -0500

    Comments to explain logic

--- apps/blockchain/contracts/Game.sol
+++ apps/blockchain/contracts/Game.sol
@@ -5,8 +5,11 @@ import "./Treasury.sol";
 import "./GambitAuthorizations.sol";
 
 /**
-    A contract that is all wired to create games on gamebit that use single moves/bets. 
-    A Play game means a player pays 
+    This is a contract to allow games to be created and played on the gamebit protocol.
+    The treasury automatically incentivizes the players and takes any fees that are owed to ensure viability of the protocol.
+    Games can be implemted in two ways. A contract can implement both but has to manage it's funds appropriately.
+     - Sessions: A time period where the game is played and the winnings are payed at the end
+     - Plays/Bets: A game where there each move/bet is a separate instance of the game. (think slots, scratch cards, combat games).
  */
 contract Game {
     ITreasury treasury;
@@ -20,14 +23,11 @@ contract Game {
         treasuryContract = payable(_treasury);
     }
 
+    // single plays methods (i.e. moves, scratch cards, etc)
+
     function acceptPlay(uint256 _amount, uint256 _profit) internal {
         contractTreasuryAccount += _amount;
         treasury.acceptPlay(_amount, _profit);
-    }
-
-    function takeGameSessionBet(uint256 _amount) internal {
-        contractTreasuryAccount -= _amount;
-        treasury.takeGameSessionBet(_amount);
     }
 
     function distributePlayWinnings(uint256 _amount, address payable _winner)
@@ -35,6 +35,13 @@ contract Game {
     {
         contractTreasuryAccount -= _amount;
         treasury.distributePlayWinnings(_amount, _winner);
+    }
+
+    // game session methods (i.e. horse race, blackjack)
+
+    function takeGameSessionBet(uint256 _amount) internal {
+        contractTreasuryAccount += _amount;
+        treasury.takeGameSessionBet(_amount);
     }
 
     function payGameSessionWinnings(
