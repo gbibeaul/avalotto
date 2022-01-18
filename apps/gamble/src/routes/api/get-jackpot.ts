0d6e38875237bc8caf88e75c4672f8fcbf1f26bf
Chinthaka <thinkaloud.lk@gmail.com>
2022-01-18 20:38:16 +0530

    post to get

---
+++ apps/gamble/src/routes/api/get-jackpot.ts
@@ -0,0 +1,26 @@
+import { supabase } from '../../transport/supabase';
+import { lottoProvider } from '../../transport/lotto';
+import { parseBearer as isBearerValid } from '../../helpers/parseBearer.helpers';
+import { utils } from 'ethers';
+
+export async function get(request) {
+	try {
+		const apiKey = await supabase.from('api_keys').select('value').eq('id', 1).limit(1);
+
+		if (!isBearerValid(request.headers.authorization, apiKey.data[0])) {
+			return {
+				message: 'not authorized',
+				status: 403
+			};
+		}
+
+		const lotto = await lottoProvider();
+		const jackpot = await lotto.getCurrentJackpot();
+
+		return parseFloat(utils.formatEther(jackpot));
+		
+	} catch (error) {
+		console.error(error);
+		return error;
+	}
+}
