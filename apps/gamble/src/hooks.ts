5c32e69e75afc13e1fbc0ea873f8eb592a73f184
Guillaume Bibeau-laviolette <g.bibeaulaviolette@gmail.com>
2022-01-27 18:38:00 +0100

    one file

--- apps/gamble/src/hooks.ts
+++ apps/gamble/src/hooks.ts
@@ -2,20 +2,21 @@ import { parse } from 'cookie';
 import { supabase } from './transport/supabase';
 
 /** @type {import('@sveltejs/kit').Handle} */
-export async function handle({ event, resolve,  }) {
-	const response = await resolve(event)
-	const cookies = parse(response.headers.cookie || '');
+export async function handle({ event, resolve }) {
+	const rawCookie = await event.request.headers.get('cookie');
+	const cookies =
+		typeof rawCookie === 'string' ? {} : parse(await event.request.headers.get('cookie'));
 
 	event.locals.walletAddress = cookies.walletAddress ? cookies.walletAddress : null;
+	const response = await resolve(event);
 
-
-	return response
+	return response;
 }
 
 /** @type {import('@sveltejs/kit').GetSession} */
-export async function getSession(request) {
-	if (request?.locals?.walletAddress) {
-		const walletAddress = request?.locals?.walletAddress;
+export async function getSession(event) {
+	if (event?.locals?.walletAddress) {
+		const walletAddress = event?.locals?.walletAddress;
 		const { data } = await supabase
 			.from('notification_targets')
 			.select()
