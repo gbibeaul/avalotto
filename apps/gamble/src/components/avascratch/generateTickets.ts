cf5877a923fe96d03cc2076c9f886a6b6ef4c136
John Flaherty <flahertyj@gmail.com>
2022-01-26 23:01:09 -0700

    Ensure at least 3 matching numbers when generating tickets

--- apps/gamble/src/components/avascratch/generateTickets.ts
+++ apps/gamble/src/components/avascratch/generateTickets.ts
@@ -2,12 +2,17 @@ export default (num) => {
 	if (num < 1) return;
 
 	const tickets = [...Array(num)].map(() => {
-		return {
+		const ticket = {
 			numbers: [...Array(9)].map(() => ({
 				number: Math.ceil(Math.random() * 99),
 				scratched: false
 			}))
 		};
+
+		ticket.numbers[1].number = ticket.numbers[0].number;
+		ticket.numbers[2].number = ticket.numbers[0].number;
+
+		return ticket;
 	});
 
 	return tickets;
