d7eeaa143f3e153af134de5b66412382b31f1dc6
Ziad Saab <ziad.saab@gmail.com>
2022-01-28 21:58:27 -0500

    fix: allow / in path of docs

--- apps/gamble/src/routes/api/get-docs/[...slug].ts
+++ apps/gamble/src/routes/api/get-docs/[...slug].ts
@@ -8,7 +8,7 @@ const docsDir = path.resolve(dirname, '..', '..', '..', 'docs');
 export async function get(req) {
   const slug = req.params.slug;
   try {
-    if (!/^[\d\w-]+$/g.test(slug)) {
+    if (!/^[\d\w-/]+$/g.test(slug)) {
       throw new Error(`Invalid slug: ${slug}`);
     }  
     const content = await fs.readFile(path.resolve(docsDir, `${slug}.md`));
