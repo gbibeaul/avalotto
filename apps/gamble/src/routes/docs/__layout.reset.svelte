61ae91aad1714a5cab70eb2a7996bd0db4818045
Ziad Saab <ziad.saab@gmail.com>
2022-01-28 22:29:20 -0500

    feat: add documentation logo

--- apps/gamble/src/routes/docs/__layout.reset.svelte
+++ apps/gamble/src/routes/docs/__layout.reset.svelte
@@ -2,8 +2,17 @@
   import './docs.css';
 </script>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<header class="p-4 bg-slate-400">
-  Documentation header!
+<style>
+  .logo {
+    width: 280px;
+  }
+</style>
+<header class="p-4 bg-slate-600">
+  <img
+    class="logo"
+    src="/assets/snowy-palace-documentation-header.png"
+    alt="Snowy Palace Documentation"
+  />
 </header>
 <main class="p-2">
   <slot></slot>
