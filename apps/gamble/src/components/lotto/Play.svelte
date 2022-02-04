140a2e8c5aa0de365707d6631bbbfe8b9a8760bd
Chinthaka Weerakkody <thinkaloud.lk@gmail.com>
2022-02-04 07:19:27 +0530

    Design fixes Lotto (web+mobile) (#128)
    * lotto-left
    
    * aligned
    
    * ticket review avax logo

--- apps/gamble/src/components/lotto/Play.svelte
+++ apps/gamble/src/components/lotto/Play.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import FaDice from 'svelte-icons/fa/FaDice.svelte';
 	import { fade } from 'svelte/transition';
-	import FaTimes from 'svelte-icons/fa/FaTimes.svelte';
+	import MDClose from 'svelte-icons/md/MdClose.svelte';
 	export let playNo: number;
 	export let num1: number;
 	export let num2: number;
 	export let num3: number;
 	export let isFirst: boolean;
-    export let removePlay: VoidFunction;
+	export let removePlay: VoidFunction;
 
 	const getRandom = () => Math.floor(Math.random() * 100);
 
@@ -25,49 +25,56 @@
 	};
 </script>
 
-<div class="container px-8 flex md:flex-row flex-col justify-center  border-b-2 drop-shadow-sm border-gray-100 " in:fade={{ duration: 750 }}>
-	<p class="w-full md:w-1/6 flex text-bold text-lg justify-center mt-8 ">PLAY {playNo}</p>
-	<div class="flex container space-x-3 justify-center">
-		<section class="flex py-4 space-x-3">
-			<p
-				class="h-16 w-16 rounded-md border-2 drop-shadow-sm border-gray-100 place-content-center flex items-center"
-			>
+<div
+	class="flex md:h-24 h-36 flex-row justify-between flex-wrap mt-3 border-b drop-shadow-sm border-[#C0C0C0] "
+	in:fade={{ duration: 750 }}
+>
+	<p class="w-full md:w-1/6 md:mt-5 flex font-bold text-lotto-black text-lg justify-center ">
+		PLAY {playNo}
+	</p>
+
+	<section class="flex  space-x-3">
+		<div class="lotto-number-container shadow-lottoNum">
+			<p class="font-lotto text-lotto-purple text-2xl font-extrabold italic ">
 				{num1}
 			</p>
-			<p
-				class="h-16 w-16 rounded-md border-2 drop-shadow-sm  border-gray-100 place-content-center flex items-center"
-			>
+		</div>
+		<div class="lotto-number-container  shadow-lottoNum rounded-md">
+			<p class="font-lotto text-lotto-purple text-2xl font-extrabold italic ">
 				{num2}
 			</p>
-			<p
-				class="h-16 w-16 rounded-md border-2  drop-shadow-sm border-gray-100 place-content-center flex items-center"
-			>
+		</div>
+		<div class="lotto-number-container  shadow-lottoNum rounded-md">
+			<p class="font-lotto text-lotto-purple text-2xl font-extrabold italic ">
 				{num3}
 			</p>
-		</section>
-		<section class="space-x-3 flex items-baseline  ">
-			<button class="h-8 w-8 icon " on:click={handleRandomNum}><FaDice /></button>
-			<button disabled={isFirst} class="h-6 w-6" on:click={removePlay} class:disabled={isFirst} class:enabled={!isFirst}><FaTimes /></button>
-		</section>
-	</div>
+		</div>
+	</section>
+	<section class="flex md:w-1/6  justify-center items-baseline mt-5">
+		<button class="h-8 w-8 icon mr-4 " on:click={handleRandomNum}><FaDice /></button>
+		<button class="h-6 w-6" on:click={removePlay} class:disabled={isFirst} class:enabled={!isFirst}
+			><MDClose /></button
+		>
+	</section>
 </div>
 
 <style>
 	.disabled {
-		opacity: 34%;
+		opacity: 0%;
 	}
 
-    .enabled {
-        color: #5046e4;
-
-    }
+	.enabled {
+		color: #5046e4;
+	}
 	.icon {
 		color: #5046e4;
 	}
-
-	.container {
+	.lotto-number-container {
 		display: flex;
-		align-items: baseline;
+		align-items: center;
 		justify-content: center;
+		width: 4rem;
+		height: 4rem;
+		border-radius: 0.375rem;
 	}
 </style>
