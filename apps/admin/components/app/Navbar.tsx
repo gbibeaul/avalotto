f4924a256011b05db3c26eba5b4432e167025cab
thehouseavax <98769585+thehouseavax@users.noreply.github.com>
2022-02-10 16:26:10 -0500

    how it works

--- apps/admin/components/app/Navbar.tsx
+++ apps/admin/components/app/Navbar.tsx
@@ -7,11 +7,8 @@ import { useAccount } from "wagmi";
 
 export const Navbar = () => {
   const [{ data: accountData }] = useAccount();
-
-  console.log(accountData);
-
   return (
-    <Disclosure as="nav">
+    <Disclosure as="nav" className="sticky top-0 z-20 bg-black">
       {({ open }) => (
         <>
           <div className="max-w-4xl mx-auto px-2 sm:px-4 md:px-8 md:max-w-5xl">
--- apps/admin/pages/index.tsx
+++ apps/admin/pages/index.tsx
@@ -2,10 +2,13 @@ import { Navbar } from "components/app/Navbar";
 import { Footer } from "components/app/Footer";
 import { Hero } from "components/app/Hero";
 import { Featured } from "components/app/Featured";
+import { Features } from "components/app/Features";
+
 const IndexPage = () => (
   <main className="bg-black f-full">
     <Navbar />
     <Hero />
+    <Features />
     <Featured />
     <Footer />
   </main>
